129a014473258e676dc2bd4770ab4da28e57b4a0
killnet73 <killnet73@gmail.com>
2022-12-15 14:21:50 +0400

    production fix

--- ikassa/settings.py
+++ ikassa/settings.py
@@ -120,6 +120,10 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR / 'static')
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://i-kassa.com/'
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
