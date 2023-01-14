d2da749389f29513ab5309d5441084ac065263c8
z4pik <segafurov@gmail.com>
2023-01-14 16:20:18 +0300

    Адаптировал под докер

--- app/ikassa/settings.py
+++ app/ikassa/settings.py
@@ -9,12 +9,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^ghv4i13ok!3$xm1%lm@q970k@0gjxqxmbxmqg2nd3ai7522%d'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['62.113.96.214']
 
 
 # Application definition
