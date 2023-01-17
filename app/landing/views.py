607074c261ea7b38b6d565e620a0afe5fa700622
z4pik <segafurov@gmail.com>
2023-01-17 10:44:37 +0300

    Parser fix

--- app/landing/views.py
+++ app/landing/views.py
@@ -8,12 +8,14 @@ class Landing(TemplateView):
     template_name = 'index.html'
 
     def get_context_data(self, **kwargs):
+        parse_data = parse()
+        print(parse_data)
         table = models.LandingCurse.objects.all()
         table_first = models.LandingCurse.objects.get(value=True)
         seotags = models.SeoTag.objects.all()
         context = super().get_context_data(**kwargs)
         context['table'] = table
         context['tablef'] = table_first
-        context['banks'] = parse()
+        context['banks'] = parse_data
         context['seotags'] = seotags
         return context
